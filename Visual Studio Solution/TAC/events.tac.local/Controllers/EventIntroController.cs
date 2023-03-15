8f1d59be8b1fdbfbba058b1f184706d36f631b92
CristiDS-Work <126245567+CristiDS-Work@users.noreply.github.com>
2023-03-15 12:06:49 +0200

    Add files via upload

---
+++ Visual Studio Solution/TAC/events.tac.local/Controllers/EventIntroController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Web.UI.WebControls;
+using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+
+namespace events.tac.local.Controllers
+{
+    public class EventIntroController : Controller
+    {
+        public static EventIntro CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            return new EventIntro()
+            {
+                ContentHeading = new HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                ContentBody = new HtmlString(FieldRenderer.Render(item, "ContentBody")),
+                EventImage = new HtmlString(FieldRenderer.Render(item, "EventImage")),
+                Highlights = new HtmlString(FieldRenderer.Render(item, "Highlights")),
+                ContentIntro = new HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+                StartDate = new HtmlString(FieldRenderer.Render(item, "StartDate")),
+                Duration = new HtmlString(FieldRenderer.Render(item, "Duration")),
+                DifficultyLevel = new HtmlString(FieldRenderer.Render(item, "DifficultyLevel"))
+            };
+        }
+
+        // GET: EventIntro
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+    }
+}
